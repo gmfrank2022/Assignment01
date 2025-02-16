7b023a92320ddd7ea8c07833d55bc4a5426a46de
gmfrank2022 <gmfrank2022@gmail.com>
2025-02-16 09:10:43 -0800

    Assignment01

---
+++ js/script.js
@@ -0,0 +1,148 @@
+
+//1. Convert the following highlighted identifiers to Camel Case notation:
+//let someMonth
+//function theMonth()
+//let currentMonth
+//let summerMonth
+//let mylibraryFunction
+
+//2. Give me an example of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression.
+//4.55 //numeric literal expression
+//'testtest' //string literal expression
+//true //Boolean literal expression
+
+//3. Give me two examples of complex / variable expressions.
+//let someMonth = 'February'
+//let currentMonth = 'January'
+
+//4. Declare (but do not assign) 9 variables for the following identifiers: 
+// First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. 
+// Use Camel Casing and Hungarian Notation when naming your identifiers.
+//firstName, lastName, strAddress, strCity, strState, zipCode, yourAge, referralSource, strMayWeContactYou
+
+//5. Take the 3 of the 9 variables that you created above and 
+// demonstrate 3 ways for declaring and assigning values to those variables.
+//let strAddress
+//let strCity
+//let zipCode
+
+//let strAddress, strCity, zipCode
+
+//const strAddress='asdfasd'
+//const strCity='fda'
+//const zipCode='12214'
+
+//6. Create a variable.
+//Add a number and a string and display the coerced result in the browser’s console window.
+//let strFirst = 'This is the '
+//let strSecond = 'nd month of the year.'
+//let iMonth = 2
+//let strConsole = strFirst + iMonth + strSecond
+//console.log(strConsole)
+
+//7. Create two variables.
+//For the first variable, add a Boolean and a string and display the coerced result.
+//For the second variable, add a number and a Boolean and display the coerced result. 
+//let boolStr = true + ' Is it correct?'
+//console.log(boolStr)
+
+//let numBool = 3 + true
+//console.log(numBool)
+
+//8. Is the following string literal valid? If not, how would you fix it?
+//let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."'
+//console.log(someString)
+//correct it into the following:
+//let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+//console.log(someString)
+
+//9. Create a variable that produces a null value in the console window.
+//Then, create a variable that produces an undefined value in the console window.
+//let x=null
+//console.log(x)
+//let x
+//console.log(x)
+
+//10. Use the unary typeof operator on various literals to return the following types within the console window: 
+// string, number, Boolean, object, and undefined.
+//typeof 'afasd'
+//typeof 12345
+//typeof true
+//let car=new Object()
+//typeof car
+//let x
+//typeof x
+
+//11. Within an alert box, use the concatenation operator (+) to display text in the alert box that appears as follows:
+//alert('Hello '+'Frank Wu'+', welcome to the JavaScript class!')
+
+//12. Declare a variable called name and set it equal to your name.
+//Substitute your name in the previous alert string with the variable instead.
+//let strName = 'Frank Wu'
+//alert('Hello '+strName+', welcome to the JavaScript class!')
+
+//13. Declare a variable called course and set it equal to “JavaScript”.
+//Rework your alert string so that it displays the string of text 
+// but using the variables as opposed to hard coded text.
+//let strName = 'Frank Wu'
+//let course = 'JavaScript'
+//alert(`Hello ${strName}, welcome to the ${course} class!`)
+
+//14. Rework the above string so that a line break is added right before the word “Welcome”. 
+//let strName = 'Frank Wu'
+//let course = 'JavaScript'
+//alert(`Hello ${strName}.\nWelcome to the ${course} class!`)
+
+//15. Replace the hardcoded string of your name with a prompt that asks the user for their name. 
+// The prompt’s response will now be captured in the name variable.
+//let strName = prompt('What\'s your name?')
+//let course = 'JavaScript'
+//alert(`Hello ${strName}.\nWelcome to the ${course} class!`)
+
+//16. Replace the hardcoded string of the class you are taking with a prompt 
+// that asks the user for the class they are taking. 
+// The prompt’s response will now be captured in the course variable.
+//let strName = prompt('What\'s your name?')
+//let course = prompt('What\'s the course?')
+//alert(`Hello ${strName}.\nWelcome to the ${course} class!`)
+
+//17. Declare a variable called x and assign it a value of 10.
+//Declare a variable called y and assign it a value of 20.
+//Display the sum of those two numbers in the console window.
+//let x = 10
+//let y = 20
+//console.log(x+y)
+
+//18. Declare a variable called x and assign it a value of 20.
+//Add and assign 20 to that variable and display the result in the console window.
+//The result should be 40.
+//let x = 20
+//x += 20
+//console.log(x)
+
+//19. Declare a variable called x and assign it a value of 20.
+//Multiply and assign 5 to that variable and display the result in the console window.
+//The result should be 100.
+//let x = 20
+//x *= 5
+//console.log(x)
+
+//20. Declare a variable called x and assign it a value that equals the remainder of 20 divided by 3.
+//Divide and assign 1 to that variable and display the result in the console window.
+//The result should be 2. If you got 6.66 try again.
+//let x = 20%3
+//x /= 1
+//console.log(x)
+
+//21. Using a set of Comparison and Logical operators, 
+// write an application that evaluates to true and displays the result within the console window.
+//let x=55
+//let y=66
+//console.log(x == 55 && y == 66)
+
+//22. Using a set of Comparison and Logical operators, 
+// write an application that evaluates to false and displays the result within the console window. 
+// The application cannot use the same operators used in the previous application.
+//let x=66
+//let y=55
+//console.log(x == 55 || y == 66)
